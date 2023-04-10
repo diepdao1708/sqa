d20136362bb122160d3c78d20239af40c4fd93c6
chungdoi <doithanhchung20011110@gmail.com>
2023-04-10 09:59:04 +0700

    cap nhat so nuoc version 2

--- src/model/Meter.java
+++ src/model/Meter.java
@@ -3,31 +3,52 @@ package model;
 
 public class Meter {
     private int meter_id;
-    private String customer_id;
+    private int customer_id;
     private String serial_number;
     private boolean status;
 
-    public Meter(int meter_id, String customer_id, String serial_number, boolean status) {
+    public Meter() {
+        
+    }
+
+    public Meter(int meter_id, int customer_id, String serial_number, boolean status) {
         this.meter_id = meter_id;
         this.customer_id = customer_id;
         this.serial_number = serial_number;
         this.status = status;
     }
+    
+    
 
     public int getMeter_id() {
         return meter_id;
     }
 
-    public String getCustomer_id() {
+    public void setMeter_id(int meter_id) {
+        this.meter_id = meter_id;
+    }
+
+    public int getCustomer_id() {
         return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
     }
 
     public String getSerial_number() {
         return serial_number;
     }
 
+    public void setSerial_number(String serial_number) {
+        this.serial_number = serial_number;
+    }
+
     public boolean isStatus() {
         return status;
     }
-    
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }  
 }
